8e7a2b35e9959c731b81f6fe614b9aafccae2af3
Boris Miranda <brmiranda009@gmail.com>
2021-01-13 10:55:09 -0600

    empezando los test

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export const GifGridItem = ({ id, title, url }) => {
 
    return (
-      <div className = "card animate__fadeOut">
-         <img src = {url} alt= {title}></img>
+      <div className="card animate__fadeOut">
+         <img src={url} alt={title}></img>
          <p>{title}</p>
       </div>
    )
+}
+GifGridItem.propTypes = {
+   title: PropTypes.string.isRequired,
+   url: PropTypes.string.isRequired,
 }
--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -3,8 +3,11 @@ import { shallow } from "enzyme";
 import { GifGridItem } from "../../components/GifGridItem";
 
 describe('pruebas en GifGridItem', () => {
+   const title = 'Un titulo';
+   const url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTXlets_YMDLRWRdARL10-uXyzSbK3v7rTgw&usqp=CAU';
+
    test('debe de mostrar el componente correctamente ', () => {
-      const wrapper = shallow(<GifGridItem />);
+      const wrapper = shallow(<GifGridItem title={title} url={url} />);
       expect(wrapper).toMatchSnapshot();
    });
 
